f1739a962afed4261977a058fdfe880c07ce873b
Juan Gamella <juangamella@gmail.com>
2021-01-26 11:53:07 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='ges',
-    version='1.0.0',
+    version='1.0.1',
     author='Juan L Gamella',
     author_email='juangamella@gmail.com',
     packages=['ges', 'ges.test', 'ges.scores'],

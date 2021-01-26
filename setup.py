5ce80b1d0864da2c4a57478f2cb0e30266d35b2f
Juan Gamella <juangamella@gmail.com>
2021-01-26 11:51:31 +0100

    Updated project url and license

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
-setup(
+setuptools.setup(
     name='ges',
     version='1.0.0',
     author='Juan L Gamella',
     author_email='juangamella@gmail.com',
     packages=['ges', 'ges.test', 'ges.scores'],
     scripts=[],
-    url='http://https://github.com/juangamella/ges',
-    license='LICENSE.txt',
+    url='https://github.com/juangamella/ges',
+    license='BSD 3-Clause License',
     description='Python implementation of the GES algorithm for causal discovery',
     long_description=open('README_pypi.md').read(),
     long_description_content_type="text/markdown",
